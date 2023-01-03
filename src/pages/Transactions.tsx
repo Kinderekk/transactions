ecf731151094f2e81a10736b03db36aab208f3c1
Marcin Imielski <mimielski@syncad.com>
2023-01-03 14:15:11 +0100

    add infinite scroll and make it working with filtering

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,29 +1,39 @@
 import { useEffect, useRef, useState } from 'react';
+import InfiniteScroll from "react-infinite-scroll-component";
 import Loader from '../components/Loader/Loader';
 import Error from '../components/Error/Error';
 import TransactionItem from '../components/TransactionItem/TransactionItem';
 import { getTransactions } from '../services/transactions';
 import { Transaction } from '../types/transaction';
-import './Transactions.scss';
 import Filter from '../components/Filter/Filter';
 import TransactionForm from '../components/TransactionForm/TransactionForm';
+import './Transactions.scss';
 
 function Transactions() {
   const dataFetchedRef = useRef(false);
+  const loaderCounter = useRef(0);
   const [transactionsToShow, setTransactionsToShow] = useState<Transaction[]>();
   const [transactions, setTransactions] = useState<Transaction[]>();
+  const [filteredTransactions, setFilteredTransactions] = useState<Transaction[]>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
 
   function filterTransactions(filterText: string) {
     if (transactions) {
+      loaderCounter.current = 0;
+      setTransactionsToShow([]);
       setIsLoading(true);
       // do not iterate if filter input is empty
       if (filterText === '') {
-        setTransactionsToShow(transactions);
+        setFilteredTransactions(transactions);
+        getMoreTransactions(transactions);
       } else {
-        setTransactionsToShow(transactions.filter((item) => 
-            item.beneficiary.toLowerCase().includes(filterText.toLowerCase())));
+        const filteredArray = transactions.filter((item) => 
+            item.beneficiary.toLowerCase().includes(filterText.toLowerCase()));
+        setFilteredTransactions(filteredArray);
+        if (filteredArray.length > 0) {
+          getMoreTransactions(filteredArray);
+        }
       }
       setIsLoading(false);
     }
@@ -32,9 +42,21 @@ function Transactions() {
   function handleTransactionDelete(id: number) {
     if (transactions && transactionsToShow) {
       const tempTransactions = transactions.filter((transaction) => transaction.id !== id)
+      const tempTransactionsToShow = transactionsToShow.filter((transaction) => transaction.id !== id)
+      if (filteredTransactions) {
+        const tempFilteredTransactions = filteredTransactions.filter((transaction) => transaction.id !== id)
+        setFilteredTransactions(tempFilteredTransactions);
+      }
       setTransactions(tempTransactions);
-      setTransactionsToShow(tempTransactions);
+      setTransactionsToShow(tempTransactionsToShow);
     }
+  }
+
+  function getMoreTransactions(transactions: Transaction[]) {
+    const maxIndex = loaderCounter.current + 20 > transactions.length ? transactions.length : loaderCounter.current + 20;
+    const slicedArray = transactions.slice(loaderCounter.current, maxIndex);
+    loaderCounter.current += 20;
+    setTransactionsToShow((prevState) => prevState ? prevState.concat(slicedArray) : slicedArray);
   }
 
   useEffect(() => {
@@ -46,7 +68,7 @@ function Transactions() {
     getTransactions()
       .then(data => {
         setTransactions(data);
-        setTransactionsToShow(data);
+        getMoreTransactions(data);
       })
       .catch(() => {
         setIsError(true);
@@ -76,15 +98,26 @@ function Transactions() {
           </div>
         </div>
         <div className="transactions-list-container">
-          <div className="transaction-list">
-            {transactionsToShow && !isLoading && !isError
-              && transactionsToShow.map((transaction) => (
-                <TransactionItem key={transaction.id} transaction={transaction} onTransactionDelete={(id) => handleTransactionDelete(id)} />
-              )
-            )}
-            {isLoading && <Loader />}
-            {isError && <Error message="There was an error when getting informations from server" />}
-          </div>
+          {transactions && transactionsToShow && !isLoading && !isError
+            && (
+              <div id="transaction-list" className="transaction-list">
+                <InfiniteScroll
+                  className="infinite-scroll"
+                  dataLength={transactionsToShow.length}
+                  next={() => getMoreTransactions(filteredTransactions ? filteredTransactions : transactions)}
+                  hasMore={filteredTransactions ? filteredTransactions.length > transactionsToShow.length : transactions.length > transactionsToShow.length }
+                  loader={<h4>Loading...</h4>}
+                  scrollableTarget="transaction-list"
+                >
+                  {transactionsToShow.map((transaction) => (
+                    <TransactionItem key={transaction.id} transaction={transaction} onTransactionDelete={(id) => handleTransactionDelete(id)} />
+                  ))}
+                </InfiniteScroll>
+              </div>
+            )
+          }
+          {isLoading && <Loader />}
+          {isError && <Error message="There was an error when getting informations from server" />}
         </div>
       </div>
     </div>
